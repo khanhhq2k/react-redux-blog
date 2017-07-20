e477a12f55b48e910cfcf9ac91ecbf7eed1ca21d
khanhhq2k <khanhhq2k@gmail.com>
2017-07-20 18:11:38 +0700

    add reducer for Posts index(doing)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import promise from 'redux-promise';
 // import App from './components/app';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
