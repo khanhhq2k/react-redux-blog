9b7ec933a6cd42e5dfe9f9d4138a00e54747003a
khanhhq2k <khanhhq2k@gmail.com>
2017-07-23 17:15:37 +0700

    posts new view

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class PostsNew extends Component {
+  render(){
+    return (
+      <div>
+        New Post
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
