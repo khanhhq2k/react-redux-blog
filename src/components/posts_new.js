96ae033d5e16eb54f7db1f8051864953c876b381
khanhhq2k <khanhhq2k@gmail.com>
2017-07-24 00:59:25 +0700

    adding redux form(in-process)

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,20 @@
 import React, { Component} from 'react';
-
+import {Field, reduxForm} from 'redux-form';
+// reduxForm is a function similar to connect redux, a connect helper
 class PostsNew extends Component {
   render(){
     return (
-      <div>
-        New Post
-      </div>
+      <form>
+        <Field
+          name='title'
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  // name for the form
+  form: 'PostsNewForm'
+})(PostsNew);
