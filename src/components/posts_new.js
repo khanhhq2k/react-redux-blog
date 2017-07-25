815bcf0aee80023b4e45376ff817c098ac4fa101
khanhhq2k <khanhhq2k@gmail.com>
2017-07-25 17:27:06 +0700

    add additional fields, dry up code

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,12 @@ import React, { Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 // reduxForm is a function similar to connect redux, a connect helper
 class PostsNew extends Component {
-  renderTitleField (field){
+  renderField (field){
     return(
-      <div>
+      <div className='form-group'>
+        <label>{field.label}</label>
         <input
+          className='form-control'
           type='text'
           {...field.input}
         />
@@ -16,8 +18,19 @@ class PostsNew extends Component {
     return (
       <form>
         <Field
+          label='Title'
           name='title'
-          component={this.renderTitleField}
+          component={this.renderField}
+        />
+        <Field
+          label='Tags'
+          name='tags'
+          component={this.renderField}
+        />
+        <Field
+          label='Post Content'
+          name='content'
+          component={this.renderField}
         />
       </form>
     );
