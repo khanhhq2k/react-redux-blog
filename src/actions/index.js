cec859d5d946c7830047145a78f183abe02cc069
khanhhq2k <khanhhq2k@gmail.com>
2017-08-02 01:29:59 +0700

    on-going

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST= 'create_post'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=khanhqkey123';
@@ -9,4 +10,12 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
