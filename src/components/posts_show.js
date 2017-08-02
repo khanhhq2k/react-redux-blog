36fb065a77274097040d2f1e99c3e5d7537e1742
khanhhq2k <khanhhq2k@gmail.com>
2017-08-02 13:40:21 +0700

    add Post Show template and route path

---
+++ src/components/posts_show.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class PostsShow extends Component {
+  render(){
+    return(
+        <div>
+          Posts Show Page!
+        </div>
+    );
+  };
+}
+
+export default PostsShow;
