f2f2811daa36933802f1faae267294fdd58cc6e9
khanhhq2k <khanhhq2k@gmail.com>
2017-08-02 15:15:26 +0700

    add fetchPost action to fetch a single post and add into state, setup reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,19 @@
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 import _ from 'lodash';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
+      // what is data?? it's axios attribute!
       return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:
+      // const post = action.payload.data
+      // const newState = {...state};
+      // newState[post.id] = post;
+
+      //ES6 way, save as above:
+      // "key interpolation"
+      return { ...state, [action.payload.data.id]: action.payload.data }
+      return newState;
     default:
       return state;
   }
