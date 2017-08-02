184f7661906e95b958666cbc9446cdfcb15c9e65
khanhhq2k <khanhhq2k@gmail.com>
2017-08-02 15:49:51 +0700

    render post detail, handle null property

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions/index';
+
 class PostsShow extends Component {
+  componentDidMount() {
+    const post_id = this.props.match.params.id //from Router
+    // or even better const {id} = this.props.match.params
+    this.props.fetchPost(post_id);
+  }
   render(){
+    const {post} = this.props;
+    //prevent null property
+    if (!post) {
+      return <div>Loading...</div>;
+    }
     return(
         <div>
-          Posts Show Page!
+          <h3>{post.title}</h3>
+          <h6>Categories: {post.categories}</h6>
+          <p>{post.content}</p>
         </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+//ownProps = this.props
+function mapStateToProps({posts}, ownProps) { //only get posts part of application state
+  return {post: posts[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, {fetchPost: fetchPost})(PostsShow);
