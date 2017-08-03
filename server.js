cb41225cbffb032965a2c41ef668f633ac01d7f7
khanhhq2k <khanhhq2k@gmail.com>
2017-08-03 16:30:03 +0700

    try setting

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/'));
 
-app.get('*', (req, res) =>{
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
+// app.get('*', (req, res) =>{
+//   res.sendFile(path.resolve(__dirname, 'index.html'));
+// });
 app.listen(port);
 
 console.log("Server Started");
